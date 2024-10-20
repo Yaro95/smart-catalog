4ab2e8b9832886f8d3f24ede30274f80590f1f5f
Yaro95 <st.yaro.t@gmail.com>
2024-10-20 17:23:48 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-<div class="mode-toggle">
-  <span>Покупатель</span>
-  <label class="switch">
-    <input type="checkbox" id="mode-switch">
-    <span class="slider round"></span>
-  </label>
-  <span>Застройщик</span>
-</div>
+
