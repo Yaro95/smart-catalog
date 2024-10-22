d161024e44b8b6b2564b2f7a4cb059c5feee2c74
Yaro95 <st.yaro.t@gmail.com>
2024-10-22 17:30:52 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,27 @@
-document.getElementById('fileUpload').addEventListener('change', function (event) {
-  const file = event.target.files[0]; // Получаем загруженный файл
-
-
+document.getElementById('fileUpload').addEventListener('change', function(event) {
+  const file = event.target.files[0];
+  const previewImage = document.getElementById('previewImage');
+  const previewVideo = document.getElementById('previewVideo');
+  
   if (file) {
-    const fileType = file.type; // Определяем тип файла
-    const reader = new FileReader();
-
-    reader.onload = function (e) {
-      if (fileType.startsWith('image/')) {
-        // Если файл - изображение
-        const previewImage = document.getElementById('previewImage');
-        const previewVideo = document.getElementById('previewVideo');
-        
+    const fileType = file.type;
+    
+    // Проверяем, является ли файл изображением
+    if (fileType.startsWith('image/')) {
+      const reader = new FileReader();
+      reader.onload = function(e) {
         previewImage.src = e.target.result;
-        previewImage.style.display = 'block'; // Показать изображение
-        previewVideo.style.display = 'none';  // Скрыть видео
-      } else if (fileType.startsWith('video/')) {
-        // Если файл - видео
-        const previewVideo = document.getElementById('previewVideo');
-        const previewImage = document.getElementById('previewImage');
-        
-        previewVideo.src = e.target.result;
-        previewVideo.style.display = 'block'; // Показать видео
-        previewImage.style.display = 'none';  // Скрыть изображение
-      }
-    };
-
-    reader.readAsDataURL(file); // Читаем содержимое файла как URL
+        previewImage.style.display = 'block'; // Показываем изображение
+        previewVideo.style.display = 'none';  // Скрываем видео, если было
+      };
+      reader.readAsDataURL(file);
+    }
+    // Проверяем, является ли файл видео
+    else if (fileType.startsWith('video/')) {
+      const videoURL = URL.createObjectURL(file);
+      previewVideo.src = videoURL;
+      previewVideo.style.display = 'block'; // Показываем видео
+      previewImage.style.display = 'none';  // Скрываем изображение, если было
+    }
   }
 });
